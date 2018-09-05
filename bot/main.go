d891fe8059aad7535b64d8c72e30e33b8263df47
Balamurali Pandranki <balamurali@live.com>
2018-09-05 21:51:47 +0530

    Implement Streaming Events

--- bot/main.go
+++ bot/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"flag"
+	"io"
 	"net/url"
+	"time"
 
 	"github.com/exchangeunion/xud-tests/bot/xudrpc"
 	"github.com/satori/go.uuid"
@@ -42,14 +44,21 @@ func main() {
 	ctx := context.Background()
 	node1 := xudrpc.NewXudClient(conn1)
 	node2 := xudrpc.NewXudClient(conn2)
-	log.Println("Trying to Get Nodes Info ---> GetInfo()")
+	log.Println("Trying to Get Nodes Info ---> GetInfo() \n")
+
 	nodeoneinfo, err := node1.GetInfo(ctx, &xudrpc.GetInfoRequest{})
 	checkErr(err)
-	log.Warningln("Node1:", nodeoneinfo)
+	log.Warningln("Node1 Info: ")
+	log.Infoln("Node Version: ", nodeoneinfo.Version)
+	log.Infoln("Node PubKey: ", nodeoneinfo.NodePubKey)
+
 	nodetwoinfo, err := node2.GetInfo(ctx, &xudrpc.GetInfoRequest{})
 	checkErr(err)
-	log.Warningln("Node2:", nodetwoinfo)
-	log.Println("Asking nodes to connect with each other ---> Connect()")
+	log.Warningln("Node2 Info:")
+	log.Infoln("Node Version: ", nodetwoinfo.Version)
+	log.Infoln("Node PubKey: ", nodetwoinfo.NodePubKey)
+
+	log.Println("Asking nodes to connect with each other ---> Connect() \n")
 	conres, err := node1.Connect(ctx, &xudrpc.ConnectRequest{NodeUri: *nodeAddr2})
 	sts, ok := status.FromError(err)
 	if !ok && sts.Code().String() != "AlreadyExists" {
@@ -58,45 +67,91 @@ func main() {
 	if conres != nil {
 		log.Println(conres)
 	} else {
-		log.Warningln("Nodes Connected to eachother successfully!")
+		log.Warningln("Nodes Connected to eachother successfully! \n")
 	}
-	//TODO implement market orders
-	log.Infoln("Placing some test orders")
-	firstOrder, err := node1.PlaceOrder(ctx, &xudrpc.PlaceOrderRequest{Price: 2000, PairId: "BTC/LTC", Quantity: -50, OrderId: uuid.NewV1().String()})
-	checkErr(err)
-	log.Println(firstOrder)
-	thirdOrder, err := node2.PlaceOrder(ctx, &xudrpc.PlaceOrderRequest{Price: 2000, PairId: "BTC/LTC", Quantity: 50, OrderId: uuid.NewV1().String()})
-	checkErr(err)
-	if thirdOrder.Matches != nil {
-		log.Println("We have some order matches:")
-		log.Println(thirdOrder.Matches)
+	//Listen to Streams
+	go listenPeerOrders(node1)
+	go listenPeerOrders(node1)
+	go listenSwaps(node1)
+	go listenSwaps(node2)
+
+	log.Infoln("Starting Test Trades \n")
+	//Indefinite
+	for {
+		//TODO implement market orders
+		log.Infoln("Placing some test orders \n")
+		firstOrder, err := node1.PlaceOrder(ctx, &xudrpc.PlaceOrderRequest{Price: 2000, PairId: "BTC/LTC", Quantity: -50, OrderId: uuid.NewV1().String()})
+		checkErr(err)
+		log.Println(firstOrder)
+		thirdOrder, err := node2.PlaceOrder(ctx, &xudrpc.PlaceOrderRequest{Price: 2000, PairId: "BTC/LTC", Quantity: 50, OrderId: uuid.NewV1().String()})
+		checkErr(err)
+		if thirdOrder.Matches != nil {
+			log.Println("We have some order matches: \n")
+			log.Println(thirdOrder.Matches)
+		}
+		if thirdOrder.RemainingOrder != nil {
+			log.Println("Remaining Order Quantity: \n")
+			log.Println(thirdOrder.RemainingOrder)
+		}
+		secondOrder, err := node1.PlaceOrder(ctx, &xudrpc.PlaceOrderRequest{Price: 2000, PairId: "BTC/LTC", Quantity: -50, OrderId: uuid.NewV1().String()})
+		checkErr(err)
+		log.Println(secondOrder)
+		log.Println("")
+		log.Infoln("Cancel the last order with ID: " + secondOrder.RemainingOrder.GetId() + "\n")
+		cancelOrder, err := node1.CancelOrder(ctx, &xudrpc.CancelOrderRequest{OrderId: secondOrder.RemainingOrder.Id, PairId: "BTC/LTC"})
+		checkErr(err)
+		if cancelOrder.Canceled {
+			log.Println("Order: " + secondOrder.RemainingOrder.Id + " Successfully cancelled!")
+		} else {
+			log.Warningln("Order: " + secondOrder.RemainingOrder.Id + " couldn't be cancelled!")
+		}
+		log.Infoln("Checking orders on connected nodes: \n")
+		nodeOneOrders, err := node1.GetOrders(ctx, &xudrpc.GetOrdersRequest{PairId: "BTC/LTC"})
+		checkErr(err)
+		log.Println(nodeOneOrders)
+		nodeTwoOrders, err := node2.GetOrders(ctx, &xudrpc.GetOrdersRequest{PairId: "BTC/LTC"})
+		checkErr(err)
+		log.Println(nodeTwoOrders)
+		time.Sleep(1000)
 	}
-	if thirdOrder.RemainingOrder != nil {
-		log.Println("Remaining Order Quantity:")
-		log.Println(thirdOrder.RemainingOrder)
-	}
-	secondOrder, err := node1.PlaceOrder(ctx, &xudrpc.PlaceOrderRequest{Price: 2000, PairId: "BTC/LTC", Quantity: -50, OrderId: uuid.NewV1().String()})
-	checkErr(err)
-	log.Println(secondOrder)
-	log.Infoln("Cancel the last order with ID: " + secondOrder.RemainingOrder.GetId())
-	cancelOrder, err := node1.CancelOrder(ctx, &xudrpc.CancelOrderRequest{OrderId: secondOrder.RemainingOrder.Id, PairId: "BTC/LTC"})
-	checkErr(err)
-	if cancelOrder.Canceled {
-		log.Println("Order:" + secondOrder.RemainingOrder.Id + " Successfully cancelled!")
-	} else {
-		log.Fatalln("Order:" + secondOrder.RemainingOrder.Id + " couldn't be cancelled!")
-	}
-	log.Infoln("Checking orders on connected nodes:")
-	nodeOneOrders, err := node1.GetOrders(ctx, &xudrpc.GetOrdersRequest{PairId: "BTC/LTC"})
-	checkErr(err)
-	log.Println(nodeOneOrders)
-	nodeTwoOrders, err := node2.GetOrders(ctx, &xudrpc.GetOrdersRequest{PairId: "BTC/LTC"})
-	checkErr(err)
-	log.Println(nodeTwoOrders)
 }
 
 func checkErr(err error) {
 	if err != nil {
 		log.Fatalln(err)
+	}
+}
+
+func listenPeerOrders(node xudrpc.XudClient) {
+	log.Infoln("Starting listening to PeerOrders")
+	orderstream, err := node.SubscribePeerOrders(context.Background(), &xudrpc.SubscribePeerOrdersRequest{})
+	checkErr(err)
+	for {
+		peerOrder, err := orderstream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.SubscribePeerOrders(_) = _, %v", node, err)
+		}
+		log.Warningln("Looks like we have a new peer order: \n")
+		log.Println(peerOrder)
+	}
+}
+
+func listenSwaps(node xudrpc.XudClient) {
+	log.Infoln("Starting listening to SwapEvents")
+	swapstream, err := node.SubscribeSwaps(context.Background(), &xudrpc.SubscribeSwapsRequest{})
+	checkErr(err)
+	for {
+		swapevent, err := swapstream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.SubscribeSwaps(_) = _, %v", node, err)
+		}
+		log.Warningln("Looks like we have a swap event: \n")
+		log.Println(swapevent)
 	}
 }
