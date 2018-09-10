1f8366b6980dcc34ce975a10dc56c6483690cfcb
Balamurali Pandranki <balamurali@live.com>
2018-09-10 22:49:16 +0530

    Update node3 pubkey

--- bot/main.go
+++ bot/main.go
@@ -20,7 +20,7 @@ import (
 var (
 	nodeAddr1 = flag.String("node1", "02b66438730d1fcdf4a4ae5d3d73e847a272f160fee2938e132b52cab0a0d9cfc6@xud1.test.exchangeunion.com:8885", "XUD node1 address in the format of pubkey@host:port.")
 	nodeAddr2 = flag.String("node2", "028599d05b18c0c3f8028915a17d603416f7276c822b6b2d20e71a3502bd0f9e0a@xud2.test.exchangeunion.com:8885", "XUD node2 address in the format of pubkey@host:port.")
-	nodeAddr3 = flag.String("node3", "028599d05b18c0c3f8028915a17d603416f7276c822b6b2d20e71a3502bd0f9e0a@xud3.test.exchangeunion.com:8885", "XUD node3 address in the format of pubkey@host:port.")
+	nodeAddr3 = flag.String("node3", "03fd337659e99e628d0487e4f87acf93e353db06f754dccc402f2de1b857a319d0@xud3.test.exchangeunion.com:8885", "XUD node3 address in the format of pubkey@host:port.")
 )
 
 func main() {
